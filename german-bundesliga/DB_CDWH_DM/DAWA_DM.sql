63198e920095236f7726f26f872a4351a730e47f
edg956 <eugeniodonaque@gmail.com>
2020-01-15 15:28:22 +0100

    Scripts and data for recreating the DBs added to repo.

---
+++ german-bundesliga/DB_CDWH_DM/DAWA_DM.sql
@@ -0,0 +1,52 @@
+-- SQL statements for creating the DM
+
+--
+-- Table structure for table `Location_DataMart`
+--
+
+CREATE TABLE `Location_DataMart` (
+  `idPlace` int(11) NOT NULL,
+  `stadiumName` varchar(45) NOT NULL,
+  `stateName` varchar(45) NOT NULL,
+  `countryName` varchar(45) NOT NULL
+);
+
+--
+-- Table structure for table `Match_results_DataMart`
+--
+
+CREATE TABLE `Match_results_DataMart` (
+  `idMatch_results` int(11) NOT NULL,
+  `idPlace` int(11) NOT NULL,
+  `idTeam` int(11) NOT NULL,
+  `idTime` int(11) NOT NULL,
+  `Goals` int(11),
+  `Goals_Against` int(11),
+  `numerGames` int(11),
+  `Win` int(11),
+  `Shots` int(11),
+  `Shots_On_Target` int(11),
+  `Red_Cards` int(11),
+  `Yellow_Cards` int(11) 
+);
+
+--
+-- Table structure for table `Team_DataMart`
+--
+
+CREATE TABLE `Team_DataMart` (
+  `idTeam` int(11) NOT NULL,
+  `name` varchar(45),
+  `divisionName` varchar(45)  
+);
+
+--
+-- Table structure for table `Time_DataMart`
+--
+
+CREATE TABLE `Time_DataMart` (
+  `idTime` int(11) NOT NULL,
+  `weekNum` int(11) NOT NULL,
+  `seasonName` varchar(45) NOT NULL,
+  `decadeName` varchar(45) NOT NULL
+);
---
+++ german-bundesliga/DB_CDWH_DM/README.txt
@@ -0,0 +1,6 @@
+In order to recreate the database, first one must create one and import the
+following scripts to create tables and indixes.
+
+1- Import first the CDWH or DM sql files.
+2- Import the CSV files contained in Data corresponding to the CDWH or the DM
+3- Import the corresponding _INDEXES sql file to add indexes
